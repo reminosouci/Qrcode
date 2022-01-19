6d4488b26817dfb80e937528dcb264de1a9e9671
Administrator <reminosouci@gmail.com>
2022-01-19 23:07:37 +1100

    v1.1 file created and appended

--- app/src/main/java/com/example/qrcode/MainActivity.java
+++ app/src/main/java/com/example/qrcode/MainActivity.java
@@ -3,9 +3,13 @@ package com.example.qrcode;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,27 +26,49 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 // implements onClickListener for the onclick behaviour of button
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    static final int READ_BLOCK_SIZE = 100;
-    Button scanBtn;
-    TextView messageText, messageFormat, concScanBar;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener, RadioGroup.OnCheckedChangeListener {
+    Button scanBtn, addBtn;
+    TextView PO, WO, concScanBar, note;
     SimpleDateFormat sdf = new SimpleDateFormat("yy-MM-dd");
     String fileDate = sdf.format(new Date()) + ".csv";
-    private Menu globalMenuItem;
-
+    String[] kit_type = { "kit n/a", "kit 1", "kit 2", "kit 3", "kit 4", "kit 5" };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        PO = findViewById(R.id.PO_text);
+        WO = findViewById(R.id.WO_text);
+        note = findViewById(R.id.note_text);
+        concScanBar = findViewById(R.id.multiLine);
+
+
         // referencing and initializing
         // the button and textviews
         scanBtn = findViewById(R.id.scanBtn);
-        messageText = findViewById(R.id.textContent);
-        messageFormat = findViewById(R.id.textFormat);
-        concScanBar = findViewById(R.id.multiLine);
+        addBtn = findViewById(R.id.addBtn);
         // adding listener to the button
         scanBtn.setOnClickListener(this);
+
+
+        Spinner spin = findViewById(R.id.sp_kit);
+        spin.setOnItemSelectedListener(this);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, kit_type);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin.setAdapter(adapter);
+
+
+
+
+        // This will get the radiogroup
+        RadioGroup rGroup = (RadioGroup)findViewById(R.id.radGroupTopBottom);
+        // This will get the radiobutton in the radiogroup that is checked
+        RadioButton checkedRadioButton = (RadioButton)rGroup.findViewById(rGroup.getCheckedRadioButtonId());
+        // This overrides the radiogroup onCheckListener
+        rGroup.setOnCheckedChangeListener(this);
+
+
+
 
         File file = new File(fileDate);
         if (!file.exists()) {
@@ -62,36 +88,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
         getMenuInflater().inflate(R.menu.menu, menu);
-        globalMenuItem = menu;
         return true;
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        globalMenuItem.findItem(R.id.scan).setVisible(false);
-        // Handle item selection
-        switch (item.getItemId()) {
-            case R.id.part_inventory:
-                Intent stillage = new Intent(this, Stillage.class);
-                startActivity(stillage);
-                break;
-            case R.id.email:
-                Intent email = new Intent(this, Email.class);
-                startActivity(email);
-                break;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-        return true;
-    }
-
 
     @Override
     public void onClick(View v) {
         try {
             FileOutputStream fileout = openFileOutput(fileDate, MODE_APPEND);
             OutputStreamWriter outputWriter = new OutputStreamWriter(fileout);
-            outputWriter.write(messageText.getText().toString() + "\n");
+            outputWriter.write(PO.getText().toString() + "\n");
             outputWriter.close();
             //Toast.makeText(getBaseContext(), "File saved successfully!",Toast.LENGTH_SHORT).show();
         } catch (Exception e) {
@@ -118,8 +123,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             } else {
                 // if the intentResult is not null we'll set
                 // the content and format of scan message
-                messageText.setText(intentResult.getContents());
-                messageFormat.setText(intentResult.getFormatName());
+                String codescanned = intentResult.getContents().toString();
+                String[] codescannedlist = codescanned.split("\\$");
+                try {
+                    WO.setText(codescannedlist[0]);
+                    PO.setText(codescannedlist[1]);
+                    note.setText(codescannedlist[2]);
+                } catch (Exception e) {
+                    System.out.println("Something went wrong.");
+                }
                 concScanBar.setText(concScanBar.getText().toString() + "\n" + intentResult.getContents());
             }
         } else {
@@ -127,5 +139,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        Object item = adapterView.getItemAtPosition(i);
+        if (item != null) {
+            Toast.makeText(MainActivity.this, item.toString(),Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(MainActivity.this, "Selected",Toast.LENGTH_SHORT).show();
+        }
+    }
 
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+        Toast.makeText(MainActivity.this, "NOT Selected",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup radioGroup, int i) {
+        RadioButton button = radioGroup.findViewById(i);
+        if(button != null && i != -1)
+        {
+            Toast.makeText(MainActivity.this, button.getText()+"\t is selected", Toast.LENGTH_SHORT).show();
+        }
+
+    }
 }
