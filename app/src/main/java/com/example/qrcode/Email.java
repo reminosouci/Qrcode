8c9338d05a39492a8c35b8b09a7450603f621661
Administrator <reminosouci@gmail.com>
2022-01-20 20:58:41 +1100

    v1.1 file created and appended

--- app/src/main/java/com/example/qrcode/Email.java
+++ app/src/main/java/com/example/qrcode/Email.java
@@ -1,11 +1,19 @@
 package com.example.qrcode;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.File;
 
 public class Email extends AppCompatActivity {
     private Menu globalMenuItem;
@@ -13,6 +21,59 @@ public class Email extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_email);
+
+
+        Button startBtn = (Button) findViewById(R.id.sendEmail);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                sendEmail();
+            }
+
+            protected void sendEmail() {
+                Log.i("Send email", "");
+                String[] TO = {""};
+                String[] CC = {""};
+                Intent emailIntent = new Intent(Intent.ACTION_SEND);
+
+                emailIntent.setData(Uri.parse("mailto:"));
+                emailIntent.setType("text/plain");
+                emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+                emailIntent.putExtra(Intent.EXTRA_CC, CC);
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Your subject");
+                emailIntent.putExtra(Intent.EXTRA_TEXT, "Email message goes here");
+                File root = Environment.getExternalStorageDirectory();
+                String pathToMyAttachedFile = "temp/attachement.xml";
+                File file = new File(root, pathToMyAttachedFile);
+                if (!file.exists() || !file.canRead()) {
+                    return;
+                }
+
+                Uri uri = Uri.fromFile(file);
+                emailIntent.putExtra(Intent.EXTRA_STREAM, uri);
+                startActivity(Intent.createChooser(emailIntent, "Pick an Email provider"));
+
+
+                try {
+                    startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+                    finish();
+                    Log.i("Finished sending email...", "");
+                } catch (android.content.ActivityNotFoundException ex) {
+                    Toast.makeText(Email.this, "There is no email client installed.", Toast.LENGTH_SHORT).show();
+                }
+
+
+
+            }
+        });
+
+
+
+
+
+
+
+
+
     }
 
     @Override
