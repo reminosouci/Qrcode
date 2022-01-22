8c3a21eca39b2d29f1e3ce79e6fa84bed5c83c8f
Administrator <reminosouci@gmail.com>
2022-01-22 19:39:01 +1100

    v1.1 file created and appended

--- app/src/main/java/com/example/qrcode/Email.java
+++ app/src/main/java/com/example/qrcode/Email.java
@@ -1,70 +1,38 @@
 package com.example.qrcode;
 
+import android.Manifest;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class Email extends AppCompatActivity {
     private Menu globalMenuItem;
+
+    // After API 23 the permission request for accessing external storage is changed
+    // Before API 23 permission request is asked by the user during installation of app
+    // After API 23 permission request is asked at runtime
+    private int EXTERNAL_STORAGE_PERMISSION_CODE = 23;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_email);
 
 
-        Button startBtn = (Button) findViewById(R.id.sendEmail);
-        startBtn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                sendEmail();
-            }
 
-            protected void sendEmail() {
-                Log.i("Send email", "");
-                String[] TO = {""};
-                String[] CC = {""};
-                Intent emailIntent = new Intent(Intent.ACTION_SEND);
-
-                emailIntent.setData(Uri.parse("mailto:"));
-                emailIntent.setType("text/plain");
-                emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
-                emailIntent.putExtra(Intent.EXTRA_CC, CC);
-                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Your subject");
-                emailIntent.putExtra(Intent.EXTRA_TEXT, "Email message goes here");
-                File root = Environment.getExternalStorageDirectory();
-                String pathToMyAttachedFile = "temp/attachement.xml";
-                File file = new File(root, pathToMyAttachedFile);
-                if (!file.exists() || !file.canRead()) {
-                    return;
-                }
-
-                Uri uri = Uri.fromFile(file);
-                emailIntent.putExtra(Intent.EXTRA_STREAM, uri);
-                startActivity(Intent.createChooser(emailIntent, "Pick an Email provider"));
-
-
-                try {
-                    startActivity(Intent.createChooser(emailIntent, "Send mail..."));
-                    finish();
-                    Log.i("Finished sending email...", "");
-                } catch (android.content.ActivityNotFoundException ex) {
-                    Toast.makeText(Email.this, "There is no email client installed.", Toast.LENGTH_SHORT).show();
-                }
-
-
-
-            }
-        });
 
 
 
@@ -75,6 +43,60 @@ public class Email extends AppCompatActivity {
 
 
     }
+
+
+
+
+    public void createfile(View view) {
+        // Requesting Permission to access External Storage
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},EXTERNAL_STORAGE_PERMISSION_CODE);
+
+
+
+
+
+
+
+
+
+
+
+
+        String editTextData = "editText.ujhgkjhgkjhgfString()";
+
+        // getExternalStoragePublicDirectory() represents root of external storage, we are using DOWNLOADS
+        // We can use following directories: MUSIC, PODCASTS, ALARMS, RINGTONES, NOTIFICATIONS, PICTURES, MOVIES
+        File folder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+
+        // Storing the data in file with name as geeksData.txt
+        File file = new File(folder, "geeksData.txt");
+        writeTextData(file, editTextData);
+    }
+    private void writeTextData(File file, String data) {
+        FileOutputStream fileOutputStream = null;
+        try {
+            fileOutputStream = new FileOutputStream(file);
+            fileOutputStream.write(data.getBytes());
+            Toast.makeText(this, "Done" + file.getAbsolutePath(), Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (fileOutputStream != null) {
+                try {
+                    fileOutputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
